7ffff746d71245bb4c4db656355ba927ca9fc89f
camilojuan <camilongo_96@hotmail.com>
2020-04-17 19:36:39 -0500

    se agrego variables 2 vez

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("este e smi mensaje");
 console.log("este e smi mensaje 2lhe");
-var numero1 = 50;
-console.log(numero1);
+var numero1;
+var numero2 = 50;
+var cadena3 = "esta es mi cadena";
+var lista = [1, 2, 3, 4, 5];
+var listastring = ["hola bb", "lindo"];
+console.log(numero2);
+console.log(cadena3);
